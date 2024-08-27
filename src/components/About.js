7e935e0f24002f3251b728e524919bc2173525c8
amanuel1876 <amanedesaleng@gmail.com>
2024-08-27 15:33:52 +0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,8 @@ const About = () => {
    </div>
    <div className="about-text">
       <h2>About <span>Me</span></h2>
-        <h4>Frontend Developer</h4>
-        <p className="text-lg">I am a passionate 4th-year Software Engineering student with <br></br>a strong interest in web development and software engineering.
+        <h4>MERN Stack  Developer</h4>
+        <p className="text-lg">I am a passionate 5th-year Software Engineering student with <br></br>a strong interest in web development and software engineering.
         I love to solve problems and create innovative solutions using<br></br> the latest technologies.
          My goal is to continually learn and <br></br>grow as a developer and contribute meaningfully to the tech industry.</p>
          <a href="/about" className="btn-box">More About Me</a>
